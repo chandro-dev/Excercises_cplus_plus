2fb7fd293ba2e2e5c01add7d008ce7a7443dff2f
alejandro <alejo_carretero@hotmail.com>
2022-05-11 14:51:46 -0500

    excercises_1

---
+++ decenas_centenas_unidades.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <iomanip>
+
+using namespace std;
+int largo;
+float centenas,decenas,unidades;
+int main (){
+	setlocale (LC_ALL, "spanish");
+	cout<<"ingrese el tamaño de su verga en cm:"<<endl;
+	cin>> largo;
+	centenas=largo/100;
+	setprecision(1);
+	cout<<centenas<<endl;
+	decenas= (largo-(centenas*100))/10;
+	setprecision(1);
+	cout<<decenas<<endl;
+	unidades=(largo-(decenas*10)-(centenas*100));
+	cout<<unidades<<endl;
+}
